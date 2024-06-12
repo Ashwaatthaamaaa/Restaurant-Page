9de5411a0dfac7539f736565ee6eccbc7b470237
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-06-12 17:18:54 +0530

    add rules for css and images

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,23 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          outputPath: 'asset/',
+          publicPath: 'asset/'
+        },
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
