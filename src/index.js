15d6cc887640fdf0af67a214ef972d743c473f96
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-06-13 15:38:29 +0530

    initial declaration for pageLoad

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import './style.css';
 
 
 function pageLoad(){
-    
+    const content = document.querySelector('#content');
+    const home = document.querySelector('#home');
+    const menu = document.querySelector('#menu');
+    const about = document.querySelector('#about');
+
+    const current = home.textContent;
+
+
+    content.appendChild(Home());
+
+
+
 }
+
+pageLoad();
